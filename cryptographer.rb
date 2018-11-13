d15533d7ad71c074a04ef5f6bef9b381d40284cb
shapovalov-k <shapovalovkv@gmail.com>
2018-11-13 12:04:58 +0100

    SHA2 method added

--- cryptographer.rb
+++ cryptographer.rb
@@ -17,15 +17,24 @@ end
 # подключаем модуль Digest
 require 'digest'
 
-puts "Введите слово или фразу для шифрования:"
-phrase_to_encrypt = STDIN.gets.chomp
+puts 'Введите слово или фразу для шифрования:'
+user_input = STDIN.gets.chomp
 
-puts "Каким способом зашифровать: 1. - MD5, 2. - SHA1"
+puts 'Каким способом зашифровать:'
+puts '1. MD5'
+puts '2. SHA1'
+puts '3. SHA2'
 encrypt_method = STDIN.gets.to_i
 
-sha256 = Digest::SHA256.hexdigest phrase_to_encrypt
-md5 = Digest::MD5.hexdigest phrase_to_encrypt
+until encrypt_method.between?(1, 3)
+  puts 'Выберите 1 или 2'
+  encrypt_method = STDIN.gets.to_i
+end
 
-puts "Вот что получилось: "
-puts "#{sha256}" if encrypt_method == 1
-puts "#{md5}" if encrypt_method == 2
+puts 'Вот что получилось:'
+
+case encrypt_method
+when 1 then puts Digest::MD5.hexdigest(user_input)
+when 2 then puts Digest::SHA1.hexdigest(user_input)
+when 3 then puts Digest::SHA2.hexdigest(user_input)
+end
